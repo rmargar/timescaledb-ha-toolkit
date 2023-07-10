b40b0a22b3f7816bed4043d1dd5a636888d07f60
Ricardo Martinez <ricardomg@riseup.net>
2023-07-10 16:29:26 +0200

    print image name

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ rm /tmp/build_args || echo OK
 env >/tmp/build_args
 echo "--build-arg \""$(cat /tmp/build_args | sed -z 's/\n/" --build-arg "/g')"IGNORE_VAR=IGNORE_VAR\"" >/tmp/build_args
 BUILD_ARGS=$(cat /tmp/build_args)
+echo $IMAGE_NAME
 COMMAND="docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME:latest -f $DOCKERFILE $BUILD_ARGS --no-cache ."
 /bin/bash -c "$COMMAND"
 echo $IMAGE_NAME
